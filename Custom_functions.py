c06bccc475f3913847dc15727662b08bc77e94a2
Ronald Han <140451609+808hanronald@users.noreply.github.com>
2024-01-18 11:45:33 -1000

    corrected

--- Custom_functions.py
+++ Custom_functions.py
@@ -332,3 +332,27 @@ def build_bow_model(text_vectorization_layer, name=None):
     bow_model.summary()
     return bow_model
 
+def evaluate_classification(model, X_train, y_train, X_test, y_test,
+                         figsize=(6,4), normalize='true', output_dict = False,
+                            cmap_train='Blues', cmap_test="Reds",colorbar=False):
+  # Get predictions for training data
+  y_train_pred = model.predict(X_train)
+  # Call the helper function to obtain regression metrics for training data
+  results_train = classification_metrics(y_train, y_train_pred, #verbose = verbose,
+                                     output_dict=True, figsize=figsize,
+                                         colorbar=colorbar, cmap=cmap_train,
+                                     label='Training Data')
+  print()
+  # Get predictions for test data
+  y_test_pred = model.predict(X_test)
+  # Call the helper function to obtain regression metrics for test data
+  results_test = classification_metrics(y_test, y_test_pred, #verbose = verbose,
+                                  output_dict=True,figsize=figsize,
+                                         colorbar=colorbar, cmap=cmap_test,
+                                    label='Test Data' )
+  if output_dict == True:
+    # Store results in a dataframe if ouput_frame is True
+    results_dict = {'train':results_train,
+                    'test': results_test}
+    return results_dict
+
